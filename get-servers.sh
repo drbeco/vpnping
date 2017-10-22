f75e0fe65fce7bfa26e1c6d85f25e74a1bfd339f
Daniel Yim <danielyim@gmail.com>
2017-10-22 00:56:17 -0700

    adding script to fetch NordVPN servers

---
+++ get-servers.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## **************************************************************************
+# *                                                                        *
+# * This program is free software; you can redistribute it and/or modify   *
+# *  it under the terms of the GNU General Public License as published by  *
+# *  the Free Software Foundation version 2 of the License.                *
+# *                                                                        *
+# * This program is distributed in the hope that it will be useful,        *
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *  GNU General Public License for more details.                          *
+# *                                                                        *
+# * You should have received a copy of the GNU General Public License      *
+# *  along with this program; if not, write to the                         *
+# *  Free Software Foundation, Inc.,                                       *
+# *  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# **************************************************************************
+
+wget https://nordvpn.com/api/files/zip -O config.zip
+unzip config.zip -d vpn
+# Remove VPNs from countries with mass surveilance programs ("Five Eyes", "Nine Eyes", "Fourteen Eyes")
+# http://www.giswatch.org/en/communications-surveillance/unmasking-five-eyes-global-surveillance-practices
+# 1. Australia
+# 2. Canada
+# 3. New Zealand
+# 4. United Kingdom
+# 5. United States of America
+# 6. Denmark
+# 7. France
+# 8. Netherlands
+# 9. Norway
+# 10. Belgium
+# 11. Germany
+# 12. Italy
+# 13. Spain
+# 14. Sweden
+find vpn -type f -name '[au|ca|nz|uk|us|dk|fr|fx|nl|an|no|be|de|it|es|se]*.ovpn' | xargs rm
